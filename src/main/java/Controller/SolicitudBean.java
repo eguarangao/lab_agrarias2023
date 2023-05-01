d9f27478e53fd4d20729a25b03dffdde1ffb3ee4
ebertguaranga <ebertguaranga@gmail.com>
2023-05-01 15:47:48 -0500

    eliminando

--- src/main/java/Controller/SolicitudBean.java
+++ src/main/java/Controller/SolicitudBean.java
@@ -20,7 +20,6 @@ public class SolicitudBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private String tipoSolicitud="lalalalala";
-    private String directorio = "/resources/pdf/";
     private String nombreArchivo;
 
     public void getTipoSolicitudes(int opcion) {
@@ -33,41 +32,5 @@ public class SolicitudBean implements Serializable {
         }
     }
 
-
-//    public void getTipoSolicitudes() {
-//            tipoSolicitud = "SOLICITUD PRACTICA DE ESTUDIANTE";
-//            System.out.println(tipoSolicitud);
-//        }
-
-
-
-
-
-//    public void handleFileUpload(FileUploadEvent event) {
-//        try {
-//            nombreArchivo = event.getFile().getFileName();
-//            InputStream inputStream = event.getFile().getInputstream();
-//            OutputStream outputStream = new FileOutputStream(new File(directorio + nombreArchivo));
-//            int read = 0;
-//            byte[] bytes = new byte[1024];
-//            while ((read = inputStream.read(bytes)) != -1) {
-//                outputStream.write(bytes, 0, read);
-//            }
-//            outputStream.flush();
-//            outputStream.close();
-//            inputStream.close();
-//            FacesMessage message = new FacesMessage("Exito", event.getFile().getFileName() + " se subió correctamente.");
-//            FacesContext.getCurrentInstance().addMessage(null, message);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            FacesMessage message = new FacesMessage("Error", event.getFile().getFileName() + " no se pudo subir.");
-//            FacesContext.getCurrentInstance().addMessage(null, message);
-//        }
-//    }
-//
-//    public void guardarArchivo() {
-//        FacesMessage message = new FacesMessage("Exito", nombreArchivo + " se guardó correctamente en " + directorio);
-//        FacesContext.getCurrentInstance().addMessage(null, message);
-//    }
 
 }
