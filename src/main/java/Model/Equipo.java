1f81cfaea44b2b1332f3f793df0024d394e10b07
Jonathan Cevallos <Jonathancevv@gmail.com>
2023-05-01 16:35:27 -0500

    Cambios solicitud bean redirección

--- src/main/java/Model/Equipo.java
+++ src/main/java/Model/Equipo.java
@@ -2,10 +2,21 @@ package Model;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class Equipo {
     private int id;
-    private String nombre;
-    private Laboratorio laboratorio;
+    private String codigo;
+    private String descripcion;
+    private String marca;
+    private String modelo;
+    private String numeroSerie;
+    private String estado;
+    private String imagen;
+    private Date fechaAdquisicion;
+    private Date FechaRegistro;
+    //Relaciones entidades
+    private Aula aula;
     private CategoriaEquipo categoriaEquipo;
 }
