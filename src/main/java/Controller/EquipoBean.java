5fc4e28bc96f4f111ce77e3b337de78b655cb815
Jimlop04 <87384300+Jimlop04@users.noreply.github.com>
2023-10-11 07:16:24 -0500

    Parte Equipos

--- src/main/java/Controller/EquipoBean.java
+++ src/main/java/Controller/EquipoBean.java
@@ -2,6 +2,10 @@ package Controller;
 
 import DAO.EquipoDAO;
 import Model.*;
+import Model.Laboratorio;
+import Model.Aula;
+import Model.Usuario;
+import Model.CategoriaEquipo;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.application.FacesMessage;
@@ -21,25 +25,99 @@ public class EquipoBean implements Serializable {
 
     private EquipoDAO DaoEquipo = new EquipoDAO();
     private List<Equipo> Listequipos;
+    List<Laboratorio> listlaboratorios;
+    List<CategoriaEquipo> listCatergoria;
+    List<Aula> listaula;
     private Equipo newEquipo;
-    private int tecnicoID = 2;
+    private boolean mostrarTablaEquipos = false;
+    private boolean botonEquipoDisabled = true;
+    private int idUsuarioSession;
 
     @PostConstruct
     public void main() {
         try {
-            this.Listequipos = new ArrayList<>();
-            Listequipos = DaoEquipo.listarEquiposPorTecnico(tecnicoID);
-            System.out.println(Listequipos);
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            Usuario usuario = (Usuario) facesContext.getExternalContext().getSessionMap().get("usuario");
+            idUsuarioSession = usuario.getId();
+            if(DaoEquipo.VerificadorAdmin(idUsuarioSession)){
+                System.out.println("Eres un administrador" + idUsuarioSession);
+
+            }
+            if (DaoEquipo.VerificadorTecnic(idUsuarioSession)){
+                ListarLaboratorios();
+
+            }
+            else{
+                System.out.println("Eres nada jejejeje");
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-    public void nuevoEquipo() {
+    public void TablaEquiposPorTecnico() throws SQLException {
+        try {
+            this.Listequipos = new ArrayList<>();
+            Listequipos = DaoEquipo.listarEquiposPorTecnico(idUsuarioSession);
+            mostrarTablaEquipos = !Listequipos.isEmpty();
+            PrimeFaces.current().ajax().update("form-equipo:selectArea", "form-equipo:dt-equipos");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void TablaEquiposPorLaboratorio() throws SQLException {
+        try {
+            this.Listequipos = new ArrayList<>();
+            Listequipos = DaoEquipo.listarEquiposPorLaboratorio(idUsuarioSession);
+            mostrarTablaEquipos = !Listequipos.isEmpty();
+            botonEquipoDisabled = Listequipos.isEmpty();
+            PrimeFaces.current().ajax().update("form-equipo:tablaEquipos", "form-equipo:dt-equipos","form-equipo:botonNewEquipo" );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ListarLaboratorios() throws SQLException {
+        this.listlaboratorios = new ArrayList<>();
+        listlaboratorios = DaoEquipo.listarlaboratoriosPorTecnico(idUsuarioSession);
+        System.out.println(listlaboratorios);
+
+    }
+
+    public void ListarCategoriaEquipo() throws SQLException {
+        this.listCatergoria = new ArrayList<>();
+        listCatergoria = DaoEquipo.listarCategorias();
+        System.out.println(listCatergoria);
+
+    }
+
+    public void ListarAulaxLaboratorio() {
+        try {
+            this.listaula = new ArrayList<>();
+            listaula = DaoEquipo.ListarAulaPorLaboratorio(idUsuarioSession);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void SelectLaboratorioTecnico() throws SQLException {
+        try {
+            TablaEquiposPorLaboratorio();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void nuevoEquipo() throws SQLException {
         this.newEquipo = new Equipo();
-        this.newEquipo.setLaboratorio(new Laboratorio());
         this.newEquipo.setCategoriaEquipo(new CategoriaEquipo());
+        this.newEquipo.setAula(new Aula());
+        ListarCategoriaEquipo();
+        ListarAulaxLaboratorio();
 
     }
 
@@ -47,24 +125,22 @@ public class EquipoBean implements Serializable {
         try {
             if (this.newEquipo.getFechaAdquisicion() == null) {
                 DaoEquipo.agregarEquipo(newEquipo);
-                Listequipos = DaoEquipo.listarEquiposPorTecnico(tecnicoID);
+                Listequipos = DaoEquipo.listarEquiposPorLaboratorio(idUsuarioSession);
                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Equipo agregado correctamente"));
-
             }
             PrimeFaces.current().executeScript("PF('manageEquipoDialog').hide()");
-            PrimeFaces.current().ajax().update("form-equipo:messages", "form-equipo:dt-equipos");
+            PrimeFaces.current().ajax().update("form-equipo:messages");
 
-        } catch (SQLException e) {
+        } catch (Exception e) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Error al agregar el Equipo"));
             e.printStackTrace();
         }
     }
-
     public void eliminarEquipo() {
         try {
             int EliminarEquipoID = newEquipo.getId();
                 DaoEquipo.eliminarEquipo(EliminarEquipoID);
-                Listequipos = DaoEquipo.listarEquiposPorTecnico(tecnicoID);
+                Listequipos = DaoEquipo.listarEquiposPorTecnico(idUsuarioSession);
                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Equipo Eliminado"));
                 PrimeFaces.current().executeScript("PF('manageEquipoDialog').hide()");
                 PrimeFaces.current().ajax().update("form-equipo:messages", "form-equipo:dt-equipos");
--- src/main/java/DAO/EquipoDAO.java
+++ src/main/java/DAO/EquipoDAO.java
@@ -91,12 +91,61 @@ public class EquipoDAO extends Conexion {
         return Listequipos;
     }
 
-    public void agregarEquipo(Equipo equipo) throws SQLException {
+    public List<Equipo> listarEquiposPorLaboratorio(int LaboID) throws SQLException {
+        List<Equipo> Listequipos = new ArrayList<>();
         this.conectar();
-        String query = "select * from laboratorio.AgregarEquipo(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String query = "select * from laboratorio.listarequiposporlaboratorio(?)";
 
-            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            preparedStatement.setInt(1, equipo.getLaboratorio().getId());
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setInt(1, LaboID);
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                while (resultSet.next()) {
+                    Equipo equipo = new Equipo();
+                    Aula aula = new Aula();
+                    CategoriaEquipo categoriaEquipo = new CategoriaEquipo();
+                    Laboratorio laboratorio = new Laboratorio();
+
+                    equipo.setId(resultSet.getInt("id"));
+
+                    laboratorio.setId(resultSet.getInt("id"));
+                    laboratorio.setNombre(resultSet.getString("nom_laboratorio"));
+
+                    aula.setId(resultSet.getInt("id_aula_equipo"));
+                    aula.setNombre(resultSet.getString("aula"));
+
+                    categoriaEquipo.setId(resultSet.getInt("id_categoria_equipo"));
+                    categoriaEquipo.setNombre(resultSet.getString("categoria"));
+
+                    equipo.setCodigo(resultSet.getString("codigo"));
+                    equipo.setDescripcion(resultSet.getString("descripcion"));
+                    equipo.setMarca(resultSet.getString("marca"));
+                    equipo.setModelo(resultSet.getString("modelo"));
+                    equipo.setNumeroSerie(resultSet.getString("num_serie"));
+                    equipo.setFechaAdquisicion(resultSet.getDate("fecha_adquisicion"));
+                    equipo.setEstado(resultSet.getBoolean("estado"));
+
+                    equipo.setAula(aula);
+                    equipo.setCategoriaEquipo(categoriaEquipo);
+                    equipo.setLaboratorio(laboratorio);
+
+                    Listequipos.add(equipo);
+                }
+                System.out.println("Ya liste equipos");
+            }
+        } finally {
+            this.desconectar();
+        }
+        return Listequipos;
+    }
+
+    public void agregarEquipo(Equipo equipo) {
+
+        this.conectar();
+        String query = "select * from laboratorio.agregarnuevoequipo(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, equipo.getAula().getId());
             preparedStatement.setInt(2, equipo.getCategoriaEquipo().getId());
             preparedStatement.setString(3, equipo.getCodigo());
             preparedStatement.setString(4, equipo.getDescripcion());
@@ -106,15 +155,14 @@ public class EquipoDAO extends Conexion {
             preparedStatement.setTimestamp(8, Timestamp.valueOf(LocalDateTime.now()));
             preparedStatement.setBoolean(9, equipo.getEstado());
 
-            preparedStatement.executeUpdate();
+            preparedStatement.executeQuery();
 
         } catch (SQLException e) {
-            throw e;
+            throw new RuntimeException(e);
         } finally {
             this.desconectar();
         }
     }
-
     public void eliminarEquipo(int equipoId) throws SQLException {
         this.conectar();
         String query = "DELETE FROM laboratorio.equipo WHERE id = ?";
@@ -134,7 +182,133 @@ public class EquipoDAO extends Conexion {
         }
     }
 
+    public List<Laboratorio> listarlaboratoriosPorTecnico(int tecnicoID) throws SQLException {
+        List<Laboratorio> ListLaboratorios = new ArrayList<>();
+        this.conectar();
+        String query = "select * from laboratorio.listarlaboratoriosPorTecnico(?)";
 
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setInt(1, tecnicoID);
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
 
+                while (resultSet.next()) {
+
+                    Laboratorio laboratorio = new Laboratorio();
+
+                    laboratorio.setId(resultSet.getInt("id_laboratorio"));
+                    laboratorio.setNombre(resultSet.getString("nom_laboratorio"));
+
+                    ListLaboratorios.add(laboratorio);
+                }
+            }
+        } finally {
+            this.desconectar();
+        }
+        return ListLaboratorios;
+    }
+
+    public List<CategoriaEquipo> listarCategorias() throws SQLException {
+        List<CategoriaEquipo> ListCategoriaEquipos = new ArrayList<>();
+        this.conectar();
+        String query = "select * from laboratorio.listarcategoriasequipos()";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                while (resultSet.next()) {
+
+                    CategoriaEquipo categoriaEquipo = new CategoriaEquipo();
+
+                    categoriaEquipo.setId(resultSet.getInt("id_categoria"));
+                    categoriaEquipo.setNombre(resultSet.getString("categoria"));
+
+                    ListCategoriaEquipos.add(categoriaEquipo);
+                }
+            }
+        } finally {
+            this.desconectar();
+        }
+        return ListCategoriaEquipos;
+    }
+
+    public List<Aula> ListarAulaPorLaboratorio(int laboratorioID) throws SQLException {
+        List<Aula> ListAulas = new ArrayList<>();
+        this.conectar();
+        String query = "select * from laboratorio.listarareasporlaboratorio(?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setInt(1, laboratorioID);
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                while (resultSet.next()) {
+
+                    Aula aula = new Aula();
+
+                    aula.setId(resultSet.getInt("id_aula"));
+                    aula.setNombre(resultSet.getString("aula"));
+
+                    ListAulas.add(aula);
+                }
+                System.out.println("Ya liste aulas");
+            }
+        } finally {
+            this.desconectar();
+        }
+        return ListAulas;
+    }
+
+    public boolean VerificadorAdmin(int ID) throws SQLException {
+        boolean verificador = false;
+
+        try {
+            this.conectar();
+            String query = "select * from laboratorio.verificaradministrador(?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, ID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                verificador = resultSet.getBoolean(1);
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            this.desconectar();
+        }
+
+        return verificador;
+    }
+
+    public boolean VerificadorTecnic(int ID) throws SQLException {
+        boolean verificador = false;
+
+        try {
+            this.conectar();
+            String query = "select * from laboratorio.verificartecnico(?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, ID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                verificador = resultSet.getBoolean(1);
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            this.desconectar();
+        }
+
+        return verificador;
+    }
 
 }
